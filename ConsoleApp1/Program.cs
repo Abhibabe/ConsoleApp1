4a87b41faa7958adb7b4261dd02b0232faef8772
hp <hp@DESKTOP-O7H8TQ6>
2022-08-17 17:03:53 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       public int Ischeck(int Num)
+        {
+
+            int i, flag = 0;
+            
+
+            for(i=1;i<=Num/2;i++)
+
+
+
+            return 0;
+        }
+        static void Main(string [] args)
+        {
+            Ischeck s = new Ischeck();
+            Console.WriteLine("Enter the NUmber ")
+             int Num = int.Parse(Console.ReadLine());
+             
+        }
+    }
+
+}
